5ee6a8bebe60edcd80efc2758814592ac474474a
MGSOPS\akshayp <akshay.pathare@derivco.co.za>
2017-12-18 11:36:06 +0200

    updating css gulp task and adding build folder

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@
  */
 var gulp = require('gulp'),
     gulpSync = require('gulp-sync')(gulp),
+    concat = require('gulp-concat');
     plumber = require('gulp-plumber');
 
 
 gulp.task('css',function () {
     return gulp.src('src/css/*.css')
         .pipe(plumber())
+        .pipe(concat('customCheckbox.css'))
         .pipe(gulp.dest('build/css'));
 });
 gulp.task('build',['sass']);
